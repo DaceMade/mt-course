997d3ed52977e5dac90e56486954c37b5ae2395b
Vadim <9536611@gmail.com>
2023-06-29 22:06:24 +0400

    2-2

---
+++ src/main/java/course/concurrency/m2_async/executors/spring/ExecutorConfiguration.java
@@ -0,0 +1,20 @@
+package course.concurrency.m2_async.executors.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class ExecutorConfiguration extends AsyncConfigurerSupport {
+    /*@Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(1);
+        executor.initialize();
+        return executor;
+    }*/
+}
